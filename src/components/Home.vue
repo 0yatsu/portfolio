90a0aa036140eb37541d4bf2259e76453502e7b5
souh <ikasumi9999999@yahoo.co.jp>
2020-05-19 01:32:02 +0900

    add skills content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div id="home" class="col-12">
     <!-- <h1>Home</h1> -->
     <div class="">
-      <p class="hometitle text-center">
+      <p class="hometitle text-center glitch" data-text="Welcome to 0yatsu portfolio site.">
         Welcome to 0yatsu portfolio site.
       </p>
     </div>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,37 +2,64 @@
   <div class="col-12" id="skills">
     <h1>Skills</h1>
     <div>
-      使ったことがある言語、フレームワーク、ツールなど一覧
+      レベル1:独学、レベル2:研究、インターン等での使用経験あり、レベル3:使いこなせる、人に教えることができる
     </div>
     <div class="">
       <div class="card col-12 mb-2">
         <div class="card-header">
-          プログラミング言語、フレームワーク
+          プログラミング言語
           <div class="card-body">
-            <ul>
-              <li>
-                C
-              </li>
-              <li>
-                C++
-              </li>
-              <li>
-                C#
-              </li>
-              <li>
-                JavaScript
-              </li>
-              <li>
-                HTML5
-              </li>
-              <li>
-                CSS3
-              </li>
-              <li>
-                HTML5
-              </li>
-
-            </ul>
+            <table class="table mb-0">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th scope="col">フレームワーク</th>
+                  <th scope="col">レベル</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">C</th>
+                  <td></td>
+                  <td>レベル2</td>
+                </tr>
+                <tr>
+                  <th scope="row">C++</th>
+                  <td></td>
+                  <td>レベル2</td>
+                </tr>
+                <tr>
+                  <th scope="row">C#</th>
+                  <td></td>
+                  <td>レベル1</td>
+                </tr>
+                <tr>
+                  <th scope="row">JavaScript</th>
+                  <td>Angular</td>
+                  <td>レベル2</td>
+                </tr>
+                <tr>
+                  <th scope="row"></th>
+                  <td>React</td>
+                  <td>レベル2</td>
+                </tr>
+                <tr>
+                  <th scope="row"></th>
+                  <td>Vue.js</td>
+                  <td>レベル1</td>
+                </tr>
+                <tr>
+                  <th scope="row">HTML5</th>
+                  <td></td>
+                  <td>レベル2</td>
+                </tr>
+                <tr>
+                  <th scope="row">CSS(SCSS)</th>
+                  <td>Bootstrap</td>
+                  <td>レベル2</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
@@ -40,31 +67,75 @@
         <div class="card-header">
           ツール、サービス
           <div class="card-body">
-            <ul>
-              <li>
-                Unity
-              </li>
-              <li>
-                Cocos creatar
-              </li>
-              <li>
-                blender
-              </li>
-              <li>
-                Slack
-              </li>
-              <li>
-                Github
-              </li>
-              <li>
-                
-              </li>
-              <li>
-                blender
-              </li>
-
-            </ul>
+            <table class="table mb-0">
+              <thead>
+                <tr>
+                  <th scope="col"></th>
+                  <th scope="col">レベル</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Unity</th>
+                  <td>レベル1</td>
+                </tr>
+                <tr>
+                  <th scope="row">Cocos creatar</th>
+                  <td>レベル2</td>
+                </tr>
+                <tr>
+                  <th scope="row">blender</th>
+                  <td>レベル1</td>
+                </tr>
+                <tr>
+                  <th scope="row">Slack</th>
+                  <td>レベル2</td>
+                </tr>
+                <tr>
+                  <th scope="row">Github</th>
+                  <td>レベル2</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="card col-12 mb-2">
+      <div class="card-header">
+        ブレイクダンス
+        <div class="card-body">
+          <table class="table mb-0">
+            <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">レベル</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">回る系</th>
+                <td>レベル1</td>
+              </tr>
+              <tr>
+                <th scope="row">逆さまになる系</th>
+                <td>レベル1</td>
+              </tr>
+              <tr>
+                <th scope="row">スレッド系</th>
+                <td>レベル3</td>
+              </tr>
+              <tr>
+                <th scope="row">フリーズ</th>
+                <td>レベル3</td>
+              </tr>
+
+              <tr>
+                <th scope="row">DJ</th>
+                <td>レベル1</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
