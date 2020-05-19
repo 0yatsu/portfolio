6847e2b0c6e1b2786349f7f196b25d3f39e1eb0e
souh <ikasumi9999999@yahoo.co.jp>
2020-05-19 15:33:48 +0900

    create contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="contact">
     <h1>Contact</h1>
-    <div class="">
-      Contact
+    <div class="row">
+      <div class="offset-4">
+        なにかありましたら、下記フォームへお気軽に連絡ください！<br>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf8DCVrqHwani7qzPFmOvrKkOnZ5s59m4NDabttDsvo0oGywA/viewform?embedded=true" width="640" height="831" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+      </div>     
     </div>
   </div>
 </template>
