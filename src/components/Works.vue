69e8a22730e4797ab7f97ee862e0f7033192cabe
souh <ikasumi9999999@yahoo.co.jp>
2020-05-19 14:46:52 +0900

    create works page

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,8 +1,61 @@
 <template>
-<div>
-    <h1>works</h1>
-    <div class="testscss">
-      works
+  <div class="col-12" id="works">
+    <h1>Works</h1>
+    <h4 class="col-12">制作物</h4>
+    <div class="row">
+      <div class="col-6">
+        <img class="aboutimg" src="../static/portfolio.png"/>
+      </div>
+      <div class="col-6">
+        ポートフォリオサイト
+        <ul>
+          <li>
+            ポートフォリオサイトが欲しくなったので作成
+          </li>
+          <li>
+            使用技術:Vue.js、Bootstrap
+          </li>
+          <li>
+            <label>
+              <font-awesome-icon icon="github" class="githubicon fa-3x p-2" />
+              <a href="https://github.com/0yatsu/portfolio">Githubリポジトリ</a>
+            </label>
+          </li>
+
+        </ul>
+      </div>
     </div>
+        <div class="row">
+      <div class="col-6">
+        <img class="aboutimg" src="../static/atchat.png"/>
+      </div>
+      <div class="col-6">
+        WEBチャットアプリ
+        <ul>
+          <li>
+            とある企業のオンラインスキルアップ課題として作成
+          </li>
+          <li>
+            使用技術:Vue.js、Bootstrap、Websocket
+          </li>
+          <li>
+            <label>
+              <font-awesome-icon icon="github" class="githubicon fa-3x p-2" />
+              <a href="https://github.com/0yatsu/online-skillup">Githubリポジトリ</a>
+            </label>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+    <!-- <div class="row">
+      <div class="col-6">
+        sukusyotoka
+      </div>
+      <div class="col-6">
+        setumei
+      </div>
+    </div> -->
+
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ require('@/assets/sass/main.scss')
 // import VueLoading from 'vue-loading-template'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { Slide } from 'vue-burger-menu'
 import App from './App.vue'
 
-library.add(fas)
+library.add(fas, fab)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 // Vue.use(VueLoading, /** options **/)
 Vue.component('slide', Slide);
