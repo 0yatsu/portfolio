6bbaaebd749aea1a2bb97833a092dd733a24b198
souh <ikasumi9999999@yahoo.co.jp>
2020-05-27 19:02:36 +0900

    add work

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -18,14 +18,14 @@
           <li>
             <label>
               <font-awesome-icon icon="github" class="githubicon fa-3x p-2" />
-              <a href="https://github.com/0yatsu/portfolio">Githubリポジトリ</a>
+              <a href="https://github.com/0yatsu/portfolio" target="_blank" rel="noopener noreferrer">Githubリポジトリ</a>
             </label>
           </li>
 
         </ul>
       </div>
     </div>
-        <div class="row">
+    <div class="row">
       <div class="col-6">
         <img class="aboutimg" src="../static/atchat.png"/>
       </div>
@@ -41,13 +41,39 @@
           <li>
             <label>
               <font-awesome-icon icon="github" class="githubicon fa-3x p-2" />
-              <a href="https://github.com/0yatsu/online-skillup">Githubリポジトリ</a>
+              <a href="https://github.com/0yatsu/online-skillup" target="_blank" rel="noopener noreferrer">Githubリポジトリ</a><br>
+              <a href="https://online-skillup-0yatsu.herokuapp.com/" target="_blank" rel="noopener noreferrer">atChat</a>
             </label>
           </li>
 
         </ul>
       </div>
     </div>
+    <div class="row">
+      <div class="col-6">
+        <img class="aboutimg" src="../static/2daction.png"/>
+      </div>
+      <div class="col-6">
+        2Dアクションゲーム
+        <ul>
+          <li>
+            YouTubeでゲーム開発をやっている動画を見て自分でもやりたくなったので作成
+          </li>
+          <li>
+            使用技術:Unity、C#、WebGL
+          </li>
+          <li>
+            <unity
+              あああ
+              src="public\unitybuild\Build\public.json"
+              unityLoader="public\unitybuild\Build\UnityLoader.js">
+            </unity>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+
     <!-- <div class="row">
       <div class="col-6">
         sukusyotoka
@@ -59,3 +85,24 @@
 
   </div>
 </template>
+
+<script>
+import unity from 'vue-unity-webgl'
+
+export default {
+  components: {
+    unity
+  },
+  data() {
+    return {
+      gameWidth: window.innerWidth,
+      gameHeight: window.innerHeight * 0.96 - 134 // 134はヘッダー等の値
+      // navinput: {
+      //   type: Boolean,
+      //   default: false,
+      // }
+    }
+  },
+
+}
+</script>
